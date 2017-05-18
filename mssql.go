rgl/dump-sql-server-certificate-chain-go-mssqldb

mssql: support OUTPUT parameters, detect PROC names
In go1.9 expect NamedValueChecker to land. Support OUTPUT
parameters and model how other types can be supported.

mssql.go
@@ -58,11 +58,18 @@ type MssqlConn struct {
 	transactionCtx context.Context
 
 	processQueryText bool
+
+	outs map[string]interface{}
+}
+
+func (c *MssqlConn) clearOuts() {
+	c.outs = nil
 }
 
 func (c *MssqlConn) simpleProcessResp(ctx context.Context) error {
 	tokchan := make(chan tokenStruct, 5)
-	go processResponse(ctx, c.sess, tokchan)
+	go processResponse(ctx, c.sess, tokchan, c.outs)
+	c.clearOuts()
 	for tok := range tokchan {
 		switch token := tok.(type) {
 		case doneStruct:
@@ -183,7 +190,7 @@ func (d *MssqlDriver) open(dsn string) (*MssqlConn, error) {
 		}
 	}
 
-	conn := &MssqlConn{sess, context.Background(), d.processQueryText}
+	conn := &MssqlConn{sess, context.Background(), d.processQueryText, nil}
 	conn.sess.log = d.log
 	return conn, nil
 }
@@ -254,7 +261,11 @@ func (s *MssqlStmt) sendQuery(args []namedValue) (err error) {
 	}
 	if s.c.sess.logFlags&logParams != 0 && len(args) > 0 {
 		for i := 0; i < len(args); i++ {
-			s.c.sess.log.Printf("\t@p%d\t%v\n", i+1, args[i])
+			if len(args[i].Name) > 0 {
+				s.c.sess.log.Printf("\t@%s\t%v\n", args[i].Name, args[i].Value)
+			} else {
+				s.c.sess.log.Printf("\t@p%d\t%v\n", i+1, args[i].Value)
+			}
 		}
 
 	}
@@ -266,25 +277,21 @@ func (s *MssqlStmt) sendQuery(args []namedValue) (err error) {
 			return driver.ErrBadConn
 		}
 	} else {
-		params := make([]Param, len(args)+2)
-		decls := make([]string, len(args))
-		params[0] = makeStrParam(s.query)
-		for i, val := range args {
-			params[i+2], err = s.makeParam(val.Value)
+		proc := Sp_ExecuteSql
+		var params []Param
+		if isProc(s.query) {
+			proc.name = s.query
+			params, _, err = s.makeRPCParams(args, 0)
+		} else {
+			var decls []string
+			params, decls, err = s.makeRPCParams(args, 2)
 			if err != nil {
 				return
 			}
-			var name string
-			if len(val.Name) > 0 {
-				name = "@" + val.Name
-			} else {
-				name = fmt.Sprintf("@p%d", val.Ordinal)
-			}
-			params[i+2].Name = name
-			decls[i] = fmt.Sprintf("%s %s", name, makeDecl(params[i+2].ti))
+			params[0] = makeStrParam(s.query)
+			params[1] = makeStrParam(strings.Join(decls, ","))
 		}
-		params[1] = makeStrParam(strings.Join(decls, ","))
-		if err = sendRpc(s.c.sess.buf, headers, Sp_ExecuteSql, 0, params); err != nil {
+		if err = sendRpc(s.c.sess.buf, headers, proc, 0, params); err != nil {
 			if s.c.sess.logFlags&logErrors != 0 {
 				s.c.sess.log.Printf("Failed to send Rpc with %v", err)
 			}
@@ -292,6 +299,39 @@ func (s *MssqlStmt) sendQuery(args []namedValue) (err error) {
 		}
 	}
 	return
+}
+
+// isProc takes the query text in s and determines if it is a stored proc name
+// or SQL text.
+func isProc(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	if s[0] == '[' && s[len(s)-1] == ']' && strings.ContainsAny(s, "\n\r") == false {
+		return true
+	}
+	return !strings.ContainsAny(s, " \t\n\r;")
+}
+
+func (s *MssqlStmt) makeRPCParams(args []namedValue, offset int) ([]Param, []string, error) {
+	var err error
+	params := make([]Param, len(args)+offset)
+	decls := make([]string, len(args))
+	for i, val := range args {
+		params[i+offset], err = s.makeParam(val.Value)
+		if err != nil {
+			return nil, nil, err
+		}
+		var name string
+		if len(val.Name) > 0 {
+			name = "@" + val.Name
+		} else {
+			name = fmt.Sprintf("@p%d", val.Ordinal)
+		}
+		params[i+offset].Name = name
+		decls[i] = fmt.Sprintf("%s %s", name, makeDecl(params[i+offset].ti))
+	}
+	return params, decls, nil
 }
 
 type namedValue struct {
@@ -325,7 +365,8 @@ func (s *MssqlStmt) queryContext(ctx context.Context, args []namedValue) (driver
 func (s *MssqlStmt) processQueryResponse(ctx context.Context) (res driver.Rows, err error) {
 	tokchan := make(chan tokenStruct, 5)
 	ctx, cancel := context.WithCancel(ctx)
-	go processResponse(ctx, s.c.sess, tokchan)
+	go processResponse(ctx, s.c.sess, tokchan, s.c.outs)
+	s.c.clearOuts()
 	// process metadata
 	var cols []columnStruct
 loop:
@@ -366,7 +407,8 @@ func (s *MssqlStmt) exec(ctx context.Context, args []namedValue) (driver.Result,
 
 func (s *MssqlStmt) processExec(ctx context.Context) (res driver.Result, err error) {
 	tokchan := make(chan tokenStruct, 5)
-	go processResponse(ctx, s.c.sess, tokchan)
+	go processResponse(ctx, s.c.sess, tokchan, s.c.outs)
+	s.c.clearOuts()
 	var rowCount int64
 	for token := range tokchan {
 		switch token := token.(type) {
@@ -518,6 +560,9 @@ func (s *MssqlStmt) makeParam(val driver.Value) (res Param, err error) {
 		return
 	}
 	switch val := val.(type) {
+	case sql.Out:
+		res, err = s.makeParam(val.Value)
+		res.Flags = fByRevValue
 	case int64:
 		res.ti.TypeId = typeIntN
 		res.buffer = make([]byte, 8)

mssql_go19.go
@@ -1,0 +1,45 @@
+// license
+
+// TODO +build go1.9
+// TODO support other data types here.
+
+// +build go1.8
+
+package mssql
+
+import (
+	"database/sql"
+	"database/sql/driver"
+
+	// "github.com/cockroachdb/apd"
+)
+
+func (c *MssqlConn) NamedValueCheck(nv *driver.NamedValue) error {
+	switch v := nv.Value.(type) {
+	case sql.Out:
+		if c.outs == nil {
+			c.outs = make(map[string]interface{})
+		}
+		c.outs[nv.Name] = v.Value
+
+		// Unwrap the Out value and check the inner value.
+		lnv := *nv
+		lnv.Value = v.Value
+		err := c.NamedValueCheck(&lnv)
+		if err != nil {
+			if err != driver.ErrSkip {
+				return err
+			}
+			lnv.Value, err = driver.DefaultParameterConverter.ConvertValue(lnv.Value)
+			if err != nil {
+				return err
+			}
+		}
+		nv.Value = sql.Out{Value: lnv.Value}
+		return nil
+	// case *apd.Decimal:
+	// 	return nil
+	default:
+		return driver.ErrSkip
+	}
+}

oneoffcode/go-mssqldb

mssql.go
@@ -58,11 +58,18 @@ type MssqlConn struct {
 	transactionCtx context.Context
 
 	processQueryText bool
+
+	outs map[string]interface{}
+}
+
+func (c *MssqlConn) clearOuts() {
+	c.outs = nil
 }
 
 func (c *MssqlConn) simpleProcessResp(ctx context.Context) error {
 	tokchan := make(chan tokenStruct, 5)
-	go processResponse(ctx, c.sess, tokchan)
+	go processResponse(ctx, c.sess, tokchan, c.outs)
+	c.clearOuts()
 	for tok := range tokchan {
 		switch token := tok.(type) {
 		case doneStruct:
@@ -183,7 +190,7 @@ func (d *MssqlDriver) open(dsn string) (*MssqlConn, error) {
 		}
 	}
 
-	conn := &MssqlConn{sess, context.Background(), d.processQueryText}
+	conn := &MssqlConn{sess, context.Background(), d.processQueryText, nil}
 	conn.sess.log = d.log
 	return conn, nil
 }
@@ -254,7 +261,11 @@ func (s *MssqlStmt) sendQuery(args []namedValue) (err error) {
 	}
 	if s.c.sess.logFlags&logParams != 0 && len(args) > 0 {
 		for i := 0; i < len(args); i++ {
-			s.c.sess.log.Printf("\t@p%d\t%v\n", i+1, args[i])
+			if len(args[i].Name) > 0 {
+				s.c.sess.log.Printf("\t@%s\t%v\n", args[i].Name, args[i].Value)
+			} else {
+				s.c.sess.log.Printf("\t@p%d\t%v\n", i+1, args[i].Value)
+			}
 		}
 
 	}
@@ -266,25 +277,21 @@ func (s *MssqlStmt) sendQuery(args []namedValue) (err error) {
 			return driver.ErrBadConn
 		}
 	} else {
-		params := make([]Param, len(args)+2)
-		decls := make([]string, len(args))
-		params[0] = makeStrParam(s.query)
-		for i, val := range args {
-			params[i+2], err = s.makeParam(val.Value)
+		proc := Sp_ExecuteSql
+		var params []Param
+		if isProc(s.query) {
+			proc.name = s.query
+			params, _, err = s.makeRPCParams(args, 0)
+		} else {
+			var decls []string
+			params, decls, err = s.makeRPCParams(args, 2)
 			if err != nil {
 				return
 			}
-			var name string
-			if len(val.Name) > 0 {
-				name = "@" + val.Name
-			} else {
-				name = fmt.Sprintf("@p%d", val.Ordinal)
-			}
-			params[i+2].Name = name
-			decls[i] = fmt.Sprintf("%s %s", name, makeDecl(params[i+2].ti))
+			params[0] = makeStrParam(s.query)
+			params[1] = makeStrParam(strings.Join(decls, ","))
 		}
-		params[1] = makeStrParam(strings.Join(decls, ","))
-		if err = sendRpc(s.c.sess.buf, headers, Sp_ExecuteSql, 0, params); err != nil {
+		if err = sendRpc(s.c.sess.buf, headers, proc, 0, params); err != nil {
 			if s.c.sess.logFlags&logErrors != 0 {
 				s.c.sess.log.Printf("Failed to send Rpc with %v", err)
 			}
@@ -292,6 +299,39 @@ func (s *MssqlStmt) sendQuery(args []namedValue) (err error) {
 		}
 	}
 	return
+}
+
+// isProc takes the query text in s and determines if it is a stored proc name
+// or SQL text.
+func isProc(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	if s[0] == '[' && s[len(s)-1] == ']' && strings.ContainsAny(s, "\n\r") == false {
+		return true
+	}
+	return !strings.ContainsAny(s, " \t\n\r;")
+}
+
+func (s *MssqlStmt) makeRPCParams(args []namedValue, offset int) ([]Param, []string, error) {
+	var err error
+	params := make([]Param, len(args)+offset)
+	decls := make([]string, len(args))
+	for i, val := range args {
+		params[i+offset], err = s.makeParam(val.Value)
+		if err != nil {
+			return nil, nil, err
+		}
+		var name string
+		if len(val.Name) > 0 {
+			name = "@" + val.Name
+		} else {
+			name = fmt.Sprintf("@p%d", val.Ordinal)
+		}
+		params[i+offset].Name = name
+		decls[i] = fmt.Sprintf("%s %s", name, makeDecl(params[i+offset].ti))
+	}
+	return params, decls, nil
 }
 
 type namedValue struct {
@@ -325,7 +365,8 @@ func (s *MssqlStmt) queryContext(ctx context.Context, args []namedValue) (driver
 func (s *MssqlStmt) processQueryResponse(ctx context.Context) (res driver.Rows, err error) {
 	tokchan := make(chan tokenStruct, 5)
 	ctx, cancel := context.WithCancel(ctx)
-	go processResponse(ctx, s.c.sess, tokchan)
+	go processResponse(ctx, s.c.sess, tokchan, s.c.outs)
+	s.c.clearOuts()
 	// process metadata
 	var cols []columnStruct
 loop:
@@ -366,7 +407,8 @@ func (s *MssqlStmt) exec(ctx context.Context, args []namedValue) (driver.Result,
 
 func (s *MssqlStmt) processExec(ctx context.Context) (res driver.Result, err error) {
 	tokchan := make(chan tokenStruct, 5)
-	go processResponse(ctx, s.c.sess, tokchan)
+	go processResponse(ctx, s.c.sess, tokchan, s.c.outs)
+	s.c.clearOuts()
 	var rowCount int64
 	for token := range tokchan {
 		switch token := token.(type) {
@@ -518,6 +560,9 @@ func (s *MssqlStmt) makeParam(val driver.Value) (res Param, err error) {
 		return
 	}
 	switch val := val.(type) {
+	case sql.Out:
+		res, err = s.makeParam(val.Dest)
+		res.Flags = fByRevValue
 	case int64:
 		res.ti.TypeId = typeIntN
 		res.buffer = make([]byte, 8)

mssql_go19.go
@@ -1,0 +1,47 @@
+// license
+
+// TODO +build go1.9
+// TODO support other data types here.
+
+// +build go1.9
+
+package mssql
+
+import (
+	"database/sql"
+	"database/sql/driver"
+
+	// "github.com/cockroachdb/apd"
+)
+
+var _ driver.NamedValueChecker = &MssqlConn{}
+
+func (c *MssqlConn) CheckNamedValue(nv *driver.NamedValue) error {
+	switch v := nv.Value.(type) {
+	case sql.Out:
+		if c.outs == nil {
+			c.outs = make(map[string]interface{})
+		}
+		c.outs[nv.Name] = v.Dest
+
+		// Unwrap the Out value and check the inner value.
+		lnv := *nv
+		lnv.Value = v.Dest
+		err := c.CheckNamedValue(&lnv)
+		if err != nil {
+			if err != driver.ErrSkip {
+				return err
+			}
+			lnv.Value, err = driver.DefaultParameterConverter.ConvertValue(lnv.Value)
+			if err != nil {
+				return err
+			}
+		}
+		nv.Value = sql.Out{Dest: lnv.Value}
+		return nil
+	// case *apd.Decimal:
+	// 	return nil
+	default:
+		return driver.ErrSkip
+	}
+}